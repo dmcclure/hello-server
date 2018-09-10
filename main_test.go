e14283228ed51694574d73041ffef04e5009bdc3
Dave McClure <dave@yougot.me>
2018-09-10 14:27:43 -0400

    Updated the test

--- main_test.go
+++ main_test.go
@@ -3,10 +3,13 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 )
 
 func TestServer(t *testing.T) {
+	os.Setenv("IMAGE_TAG", "abcdefg")
+
 	req, err := http.NewRequest("GET", "/TestName", nil)
 	if err != nil {
 		t.Fatal(err)
@@ -20,7 +23,7 @@ func TestServer(t *testing.T) {
 		t.Fatalf("Received non-200 response code: %d\n", resp.Code)
 	}
 
-	expected := "Hello TestName! ENV_NAME: test"
+	expected := "Hello TestName! IMAGE_TAG=abcdefg ENV_NAME=test"
 	if resp.Body.String() != expected {
 		t.Errorf("Expected: '%s' but received: '%s'\n", expected, resp.Body.String())
 	}
