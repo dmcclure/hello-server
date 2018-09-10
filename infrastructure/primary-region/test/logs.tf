b80ca842c9b27d7a01eb67d5bb17359195984e87
Dave McClure <dave@yougot.me>
2018-09-10 13:54:39 -0400

    Not actually using this log stream anywhere

--- infrastructure/primary-region/test/logs.tf
+++ infrastructure/primary-region/test/logs.tf
@@ -3,8 +3,3 @@ resource "aws_cloudwatch_log_group" "app" {
   name              = "hello-server-${var.environment}"
   retention_in_days = 30
 }
-
-resource "aws_cloudwatch_log_stream" "app" {
-  name           = "hello-server-${var.environment}"
-  log_group_name = "${aws_cloudwatch_log_group.app.name}"
-}
