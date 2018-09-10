de2de891585812bb7180fab60eda11809838d2f1
Dave McClure <dave@yougot.me>
2018-09-10 13:41:14 -0400

    Added some simple logging

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 	"strings"
 )
 
 func sayHelloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Handling request to " + r.URL.RequestURI())
+
 	message := r.URL.Path
 	message = strings.TrimPrefix(message, "/")
 	message = "Hello " + message + "! ENV_NAME: " + os.Getenv("ENV_NAME")
@@ -15,6 +18,8 @@ func sayHelloHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", sayHelloHandler)
+
+	fmt.Println("hello-server starting. ENV_NAME: " + os.Getenv("ENV_NAME"))
 
 	if err := http.ListenAndServe(":9090", nil); err != nil {
 		panic(err)
