218af53f35e7061ce03985ef017218c9b023fcb3
Dave McClure <dave@yougot.me>
2018-09-13 13:43:40 -0400

    Adding a load generator to test auto scaling

--- main.go
+++ main.go
@@ -4,20 +4,47 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"runtime"
 	"strings"
+	"time"
 )
 
 func sayHelloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Handling request to " + r.URL.RequestURI())
 
 	message := r.URL.Path
-	message = strings.TrimPrefix(message, "/")
+	message = strings.TrimPrefix(message, "/hello/")
 	message = "Hello " + message + "! IMAGE_TAG=" + os.Getenv("IMAGE_TAG") + " ENV_NAME=" + os.Getenv("ENV_NAME")
 	w.Write([]byte(message))
 }
 
+func loadHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Handling request to " + r.URL.RequestURI())
+
+	done := make(chan int)
+
+	for i := 0; i < runtime.NumCPU(); i++ {
+		go func() {
+			for {
+				select {
+				case <-done:
+					return
+				default:
+				}
+			}
+		}()
+	}
+
+	time.Sleep(time.Minute * 5)
+	close(done)
+
+	message := "Finished generating load for 5 minutes"
+	w.Write([]byte(message))
+}
+
 func main() {
-	http.HandleFunc("/", sayHelloHandler)
+	http.HandleFunc("/hello/", sayHelloHandler)
+	http.HandleFunc("/load", loadHandler)
 
 	fmt.Println("hello-server starting. IMAGE_TAG=" + os.Getenv("IMAGE_TAG") + " ENV_NAME=" + os.Getenv("ENV_NAME") + " BUILD_DATE=" + os.Getenv("BUILD_DATE"))
 
